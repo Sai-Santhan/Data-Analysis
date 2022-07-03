30ae33d6c75424a3bbf833c76d4b6f639e3f23ef
Sai Santhan <sai@Sais-MacBook-Pro.local>
2022-07-04 02:31:21 +0530

    Modified the Dockerfile (image size got bigger), added packages to requirements.txt and added kubernetes object for the django project

--- src/data_analysis/settings.py
+++ src/data_analysis/settings.py
@@ -24,9 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get("DEBUG")) == "1"	
 
-ALLOWED_HOSTS = []
+# Allowed_hosts	
+ALLOWED_HOSTS = []	
+if ENV_ALLOWED_HOST := os.environ.get("ENV_ALLOWED_HOST", None):	
+    ALLOWED_HOSTS = [ENV_ALLOWED_HOST]
 
 
 # Application definition
@@ -81,6 +84,27 @@ DATABASES = {
     }
 }
 
+DB_USERNAME = os.environ.get("POSTGRES_USER")	
+DB_PASSWORD = os.environ.get("POSTGRES_PASSWORD")	
+DB_DATABASE = os.environ.get("POSTGRES_DB")	
+DB_HOST = os.environ.get("POSTGRES_HOST")	
+DB_PORT = os.environ.get("POSTGRES_PORT")	
+DB_IS_AVAIL = all([DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_HOST, DB_PORT])	
+DB_IGNORE_SSL = os.environ.get("DB_IGNORE_SSL") == "true"
+
+if DB_IS_AVAIL:	
+    DATABASES = {	
+        "default": {	
+            "ENGINE": "django.db.backends.postgresql",	
+            "NAME": DB_DATABASE,	
+            "USER": DB_USERNAME,	
+            "PASSWORD": DB_PASSWORD,	
+            "HOST": DB_HOST,	
+            "PORT": DB_PORT,	
+        }	
+    }	
+    if not DB_IGNORE_SSL:	
+        DATABASES["default"]["OPTIONS"] = {"sslmode": "require"}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
---
+++ src/deployment-guide.md
@@ -0,0 +1,57 @@
+1. Test Django
+
+```
+python manage.py test
+```
+
+2. Build Container
+
+```
+docker build -f Dockerfile \
+    -t registry.digitalocean.com/my-private-registry333/data-analysis-web:latest \
+    -t registry.digitalocean.com/my-private-registry333/data-analysis-web:v1 \
+    .
+```
+
+3. Push this container to DO Container Registry
+
+```
+docker push registry.digitalocean.com/my-private-registry333/data-analysis-web --all-tags
+```
+
+4. Update secrets
+
+```
+kubectl delete secret data-analysis-web-prod-env
+kubectl create secret generic data-analysis-web-prod-env --from-env-file=web/.env.prod
+```
+
+5. Update Deployment
+
+```
+kubectl apply -f k8s/apps/data-analysis-web.yaml
+```
+
+6. Wait for Rollout to finish
+
+```
+kubectl rollout status deployment/data-analysis-web-deployment
+```
+
+7. Get the Pod name to Migrate it's Database
+
+```
+export SINGLE_POD_NAME=$(kubectl get pod -l app=data-analysis-web-deployment -o jsonpath="{.items[0].metadata.name}")
+```
+
+or
+
+```
+export SINGLE_POD_NAME=$(kubectl get pod -l app=data-analysis-web-deployment -o NAME | tail -n 1
+```
+
+8. Run the Migrations on the Pod
+
+```
+kubectl exec -it $SINGLE_POD_NAME -- bash /app/migrate.sh
+```
