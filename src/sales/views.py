6dbf7dff3f941c16d5a75254439e96404d4c9173
Sai-Santhan <dsaisanthan@gmail.com>
2022-07-14 17:31:17 +0530

    Updated sales-home template to display the converted dataframes as HTML

--- src/sales/views.py
+++ src/sales/views.py
@@ -2,17 +2,27 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Sale
 from .forms import SalesSearchForm
+import pandas as pd
 
 
 # Create your views here.
 def home_view(request):
+    sales_df = None
     form = SalesSearchForm(request.POST or None)
     if request.method == "POST":
         date_from = request.POST.get("date_from")
         date_to = request.POST.get("date_to")
         chart_type = request.POST.get("chart_type")
+
+        sale_qs = Sale.objects.filter(
+            created__date__lte=date_to, created__date__gte=date_from
+        )
+        if len(sale_qs) > 0:
+            sales_df = pd.DataFrame(sale_qs.values())
+            sales_df = sales_df.to_html()
     context = {
         'form': form,
+        'sales_df': sales_df,
     }
     return render(request, 'sales/home.html', context)
 
