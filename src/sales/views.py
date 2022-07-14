c05fec7e6fb78825ddbddafebab78d4b22430405
Sai-Santhan <dsaisanthan@gmail.com>
2022-07-14 16:54:06 +0530

    Modified the home_view of sales app to get the data from it's search form

--- src/sales/views.py
+++ src/sales/views.py
@@ -7,9 +7,11 @@ from .forms import SalesSearchForm
 # Create your views here.
 def home_view(request):
     form = SalesSearchForm(request.POST or None)
-    hello = 'HELLO WORLD!'
+    if request.method == "POST":
+        date_from = request.POST.get("date_from")
+        date_to = request.POST.get("date_to")
+        chart_type = request.POST.get("chart_type")
     context = {
-        'hello': hello,
         'form': form,
     }
     return render(request, 'sales/home.html', context)
