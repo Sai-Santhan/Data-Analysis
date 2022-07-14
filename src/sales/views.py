bd6640f2d32cdb9aef0048c8f8b2a5ff9db4fc4f
Sai-Santhan <dsaisanthan@gmail.com>
2022-07-14 16:49:57 +0530

    Created the sales search form

--- src/sales/views.py
+++ src/sales/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Sale
+from .forms import SalesSearchForm
 
 
 # Create your views here.
 def home_view(request):
-    return render(request, 'sales/home.html', {})
+    form = SalesSearchForm(request.POST or None)
+    hello = 'HELLO WORLD!'
+    context = {
+        'hello': hello,
+        'form': form,
+    }
+    return render(request, 'sales/home.html', context)
 
 
 class SaleListView(ListView):
