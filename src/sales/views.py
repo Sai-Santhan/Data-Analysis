dd1deb94de37e9894a594d1d59e38bb7cb292cd9
Sai-Santhan <dsaisanthan@gmail.com>
2022-10-31 16:36:29 +0530

    Table styling and sales_df formatting.

--- src/sales/views.py
+++ src/sales/views.py
@@ -5,19 +5,14 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
 from reports.forms import ReportForm
-from .forms import SalesSearchForm
-from .models import Sale
-from .utils import get_customer_from_id, get_salesman_from_id, get_chart
+from sales.forms import SalesSearchForm
+from sales.models import Sale
+from sales.utils import get_customer_from_id, get_salesman_from_id, get_chart
 
 
 @login_required()
 def home_view(request):
-    sales_df = None
-    positions_df = None
-    merged_df = None
-    df = None
-    chart = None
-    no_data = None
+    sales_df = positions_df = merged_df = df = chart = no_data = None
 
     search_form = SalesSearchForm(request.POST or None)
     report_form = ReportForm()
@@ -29,13 +24,14 @@ def home_view(request):
         results_by = request.POST.get("results_by")
 
         sale_qs = Sale.objects.filter(
-            created__date__lte=date_to, created__date__gte=date_from
+            created__date__gte=date_from, created__date__lte=date_to
         )
         if len(sale_qs) > 0:
             sales_df = pd.DataFrame(sale_qs.values())
             sales_df["customer_id"] = sales_df["customer_id"].apply(get_customer_from_id)
             sales_df["salesman_id"] = sales_df["salesman_id"].apply(get_salesman_from_id)
-            sales_df["created"] = sales_df["created"].apply(lambda x: x.strftime("%Y-%m-%d"))
+            sales_df["created"] = sales_df["created"].apply(lambda x: x.strftime("%Y-%m-%d %H:%M"))
+            sales_df["updated"] = sales_df["updated"].apply(lambda x: x.strftime("%Y-%m-%d %H:%M"))
             sales_df.rename({
                 "customer_id": "customer",
                 "salesman_id": "salesman",
@@ -58,18 +54,21 @@ def home_view(request):
 
             positions_df = pd.DataFrame(positions_data)
             merged_df = pd.merge(sales_df, positions_df, on='sales_id')
-
             df = merged_df.groupby('transaction_id', as_index=False)['price'].agg("sum")
 
             chart = get_chart(chart_type, sales_df, results_by)
 
-            sales_df = sales_df.to_html()
-            positions_df = positions_df.to_html()
-            merged_df = merged_df.to_html()
-            df = df.to_html()
+            sales_df = sales_df.to_html(justify='left', index=False,
+                                        classes='table table-bordered table-hover table-sm')
+            positions_df = positions_df.to_html(justify='left', index=False,
+                                                classes='table table-bordered table-hover table-sm')
+            merged_df = merged_df.to_html(justify='left', index=False,
+                                          classes='table table-bordered table-hover table-sm')
+            df = df.to_html(justify='left', index=False,
+                            classes='table table-bordered table-hover table-sm')
 
         else:
-            no_data = "No data is available in this date range"
+            no_data = "No Data is available in this Date range."
     context = {
         'search_form': search_form,
         'report_form': report_form,
