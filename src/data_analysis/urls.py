9a6dcbd405d7db2ccb511ccd38c881dc68cd697c
Sai-Santhan <dsaisanthan@gmail.com>
2022-11-02 22:28:00 +0530

    Fix Login and Sign up.

--- src/data_analysis/urls.py
+++ src/data_analysis/urls.py
@@ -18,11 +18,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import login_view, logout_view
+from .views import login_view, logout_view, signup_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sales.urls', namespace='sales')),
+    path('signup/', signup_view, name='signup'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('reports/', include('reports.urls', namespace='reports')),
--- src/data_analysis/views.py
+++ src/data_analysis/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render, redirect
 
 
@@ -9,25 +9,52 @@ def logout_view(request):
 
 
 def login_view(request):
-    error_message = None
-    form = AuthenticationForm()
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
+    if request.user.is_authenticated:
+        return redirect('sales:home')
+    else:
+        error_message = None
+        form = AuthenticationForm()
+        if request.method == 'POST':
+            form = AuthenticationForm(data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    if request.GET.get('next'):
+                        return redirect(request.GET.get("next"))
+                    else:
+                        return redirect("sales:home")
+            else:
+                error_message = "Oops ... Something went Wrong!"
+        context = {
+            "form": form,
+            "error_message": error_message
+        }
+
+        return render(request, "auth/login.html", context)
+
+
+def signup_view(request):
+    if request.user.is_authenticated:
+        return redirect('sales:home')
+    else:
+        error_message = None
+        form = UserCreationForm()
+        if request.method == "POST":
+            form = UserCreationForm(data=request.POST)
+            if form.is_valid():
+                form.save()
+                # login(request, user)
                 if request.GET.get('next'):
                     return redirect(request.GET.get("next"))
                 else:
                     return redirect("sales:home")
-        else:
-            error_message = "Oops ... Something went Wrong!"
-    context = {
-        "form": form,
-        "error_message": error_message
-    }
-
-    return render(request, "auth/login.html", context)
+            else:
+                error_message = "Oops ... Something went Wrong!"
+        context = {
+            "form": form,
+            "error_message": error_message
+        }
+        return render(request, "auth/signup.html", context)
