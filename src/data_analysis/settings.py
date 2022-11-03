4465fbbaae4b863a7f072b56bf81960316ec00b7
Sai-Santhan <dsaisanthan@gmail.com>
2022-11-03 12:21:54 +0530

    Remove USE_SPACES.

--- src/data_analysis/settings.py
+++ src/data_analysis/settings.py
@@ -187,30 +187,30 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 USE_SPACES = os.environ.get('USE_SPACES')
 
-if USE_SPACES:
-    # settings
-    AWS_ACCESS_KEY_ID = os.environ.get('STATIC_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('STATIC_SECRET_KEY')
-    AWS_STORAGE_BUCKET_NAME = os.environ.get('STATIC_BUCKET_NAME')
-    AWS_DEFAULT_ACL = 'public-read'
-    AWS_S3_ENDPOINT_URL = os.environ.get('STATIC_ENDPOINT_URL')
-    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-    # static settings
-    AWS_LOCATION = 'static'
-    STATIC_URL = f'{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
-    STATICFILES_STORAGE = 'data_analysis.storages.StaticStorage'
-    # media settings
-    PUBLIC_MEDIA_LOCATION = 'media'
-    MEDIA_URL = f'{AWS_S3_ENDPOINT_URL}/{PUBLIC_MEDIA_LOCATION}/'
-    DEFAULT_FILE_STORAGE = 'data_analysis.storages.MediaStorage'
-
-# TODO - Should we make the staticfiles and mediafiles directory beforehand
-
-else:
-    STATIC_URL = 'static/'
-    STATIC_ROOT = BASE_DIR / 'staticfiles'
-    MEDIA_URL = 'media/'
-    MEDIA_ROOT = BASE_DIR / 'media'
+# if USE_SPACES:
+#     # settings
+#     AWS_ACCESS_KEY_ID = os.environ.get('STATIC_ACCESS_KEY_ID')
+#     AWS_SECRET_ACCESS_KEY = os.environ.get('STATIC_SECRET_KEY')
+#     AWS_STORAGE_BUCKET_NAME = os.environ.get('STATIC_BUCKET_NAME')
+#     AWS_DEFAULT_ACL = 'public-read'
+#     AWS_S3_ENDPOINT_URL = os.environ.get('STATIC_ENDPOINT_URL')
+#     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+#     # static settings
+#     AWS_LOCATION = 'static'
+#     STATIC_URL = f'{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
+#     STATICFILES_STORAGE = 'data_analysis.storages.StaticStorage'
+#     # media settings
+#     PUBLIC_MEDIA_LOCATION = 'media'
+#     MEDIA_URL = f'{AWS_S3_ENDPOINT_URL}/{PUBLIC_MEDIA_LOCATION}/'
+#     DEFAULT_FILE_STORAGE = 'data_analysis.storages.MediaStorage'
+#
+# # TODO - Should we make the staticfiles and mediafiles directory beforehand
+#
+# else:
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_DIRS = [BASE_DIR / 'static',
                     BASE_DIR / 'sales' / 'static',
