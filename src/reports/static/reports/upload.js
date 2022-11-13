de23a9437cde6508154bbfad698fc6261bdab337
Sai-Santhan <dsaisanthan@gmail.com>
2022-11-13 16:59:31 +0530

    Used 'loc' variable from JsonResponse of csv_upload_view to generate different alerts.

--- src/reports/static/reports/upload.js
+++ src/reports/static/reports/upload.js
@@ -12,20 +12,22 @@ const handleAlerts = (type, msg) => {
 
 Dropzone.autoDiscover = false;
 const myDropzone = new Dropzone("#my-dropzone", {
-  url: url + "/reports/upload/",
-  init: function () {
-    this.on("sending", function (file, xhr, formData) {
-      formData.append("csrfmiddlewaretoken", csrf);
-    });
-    this.on("success", function (file, response) {
-      if (response.error) {
-        handleAlerts("danger", "File already exists!");
-      } else {
-        handleAlerts("success", "Your File has been uploaded successfully");
-      }
-    });
-  },
-  maxFiles: 3,
-  maxFilesize: 3,
-  acceptedFiles: ".csv",
+    url: url + "/reports/upload/",
+    init: function () {
+        this.on("sending", function (file, xhr, formData) {
+            formData.append("csrfmiddlewaretoken", csrf);
+        });
+        this.on("success", function (file, response) {
+            if (response.loc === 1) {
+                handleAlerts("danger", "File has already been uploaded!");
+            } else if (response.loc === 2) {
+                handleAlerts("danger", "Some other user has already uploaded the same file! (Note: Because this is a Multi-tenancy application with shared storage.)");
+            } else if (response.loc === 3) {
+                handleAlerts("success", "Your File has been uploaded successfully");
+            }
+        });
+    },
+    maxFiles: 3,
+    maxFilesize: 3,
+    acceptedFiles: ".csv",
 });
