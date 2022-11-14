cb11ecaa6d818b56634564eaccdf05122cf06d03
Sai-Santhan <dsaisanthan@gmail.com>
2022-11-14 12:37:23 +0530

    custom upload_to.

--- src/products/models.py
+++ src/products/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
 
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/products/<name>_<filename>
+    return f'products/{instance.name}/{filename}'
+
+
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=120)
-    image = models.ImageField(upload_to="products", default="no_image.webp")
+    image = models.ImageField(upload_to=user_directory_path, default="no_image.webp")
     price = models.FloatField(help_text="in US dollars $")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
--- src/profiles/models.py
+++ src/profiles/models.py
@@ -2,11 +2,16 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/csv/user_<id>/<filename>
+    return f'avatars/user_{instance.user.id}/{filename}'
+
+
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(default="no bio...")
-    avatar = models.ImageField(upload_to="avatars", default="no_picture.png")
+    avatar = models.ImageField(upload_to=user_directory_path, default="no_picture.png")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
