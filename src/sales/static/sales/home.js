1208d8e37c74e8380de843c246f8d5d67a9e5085
Sai-Santhan <dsaisanthan@gmail.com>
2022-11-14 12:49:53 +0530

    Added logic for generating report form img inside modal.

--- src/sales/static/sales/home.js
+++ src/sales/static/sales/home.js
@@ -1,30 +1,46 @@
-const reportBtn = document.getElementById("report-btn");
-const img = document.getElementById("img");
-const modalBody = document.getElementById("modal-body");
-const reportForm = document.getElementById("report-form");
-const alertBox = document.getElementById("alert-box");
 const url = window.location.origin;
+const csrf = document.getElementsByName("csrfmiddlewaretoken")[0].value;
+
+const reportImg = document.getElementById("report-img");
+const reportBtn = document.getElementById("report-btn");
+
+const modalBody = document.getElementById("modal-body");
+let reportFormImg;
+const alertBox = document.getElementById("alert-box");
+const reportForm = document.getElementById("report-form");
 
 const reportName = document.getElementById("id_name");
 const reportRemarks = document.getElementById("id_remarks");
-const csrf = document.getElementsByName("csrfmiddlewaretoken")[0].value;
 
 const handleAlerts = (type, msg) => {
-    alertBox.innerHTML = `
-    <div class="alert alert-${type}" role="alert">
-      ${msg}
-    </div>
-  `;
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${msg}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+    alertBox.append(wrapper)
 };
 
-if (img) {
+if (reportImg) {
     reportBtn.classList.remove("not-visible");
 }
 
 reportBtn.addEventListener("click", () => {
 
-    img.setAttribute("class", "w-100");
-    modalBody.prepend(img);
+    if (reportFormImg) {
+        if (reportFormImg.src !== reportImg.src) {
+            reportFormImg.remove()
+            let reportFormImg = reportImg.cloneNode(true);
+            reportFormImg.setAttribute("class", "w-100");
+            modalBody.prepend(reportFormImg)
+        }
+    } else {
+        reportFormImg = reportImg.cloneNode(true);
+        reportFormImg.setAttribute("class", "w-100");
+        modalBody.prepend(reportFormImg)
+    }
 
     reportForm.addEventListener("submit", (e) => {
         e.preventDefault();
@@ -32,7 +48,7 @@ reportBtn.addEventListener("click", () => {
         formData.append("csrfmiddlewaretoken", csrf);
         formData.append("name", reportName.value);
         formData.append("remarks", reportRemarks.value);
-        formData.append("image", img.src);
+        formData.append("image", reportImg.src);
 
         $.ajax({
             type: "POST",
@@ -43,10 +59,10 @@ reportBtn.addEventListener("click", () => {
                 reportForm.reset();
             },
             error: function (error) {
-                handleAlerts("danger", "Oops.. Something went wrong!");
+                handleAlerts("danger", "Oops.. Something went wrong! (Note: Give a unique name.)");
             },
             processData: false,
             contentType: false,
         });
-    });
-});
+    })
+})
