3c9688b8d051ada0a74691d4f9f44dcaff996860
Sai-Santhan <dsaisanthan@gmail.com>
2022-11-14 12:43:13 +0530

    'upload_to' callable to return 'customers/<username>_<filename>'

--- src/customers/models.py
+++ src/customers/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/customers/username_<filename>
+    return f'customers/{instance.name}_{filename}'
 
 
 class Customer(models.Model):
     name = models.CharField(max_length=120)
-    logo = models.ImageField(upload_to='customers/', default='no_picture.jpeg')
+    logo = models.ImageField(upload_to=user_directory_path, default='no_picture.jpeg')
 
     def __str__(self):
         return str(self.name)
