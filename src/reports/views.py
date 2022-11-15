07e82db050d126b2d9fa2104fb8704f5477ad5dd
Sai-Santhan <dsaisanthan@gmail.com>
2022-11-15 13:53:35 +0530

    Fix: img src to 'obj.image.url' and added styling.

--- src/reports/views.py
+++ src/reports/views.py
@@ -128,8 +128,7 @@ def create_report_view(request):
 def render_pdf_view(request, pk):
     template_path = "reports/pdf.html"
     obj = get_object_or_404(Report, pk=pk)
-    img = smart_open(obj.image.url)
-    context = {"obj": obj, "img": img}
+    context = {"obj": obj}
     # Create a Django response object, and specify content_type as pdf
     response = HttpResponse(content_type="application/pdf")
     # If Download
